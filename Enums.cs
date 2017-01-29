102ff43a3e643af2d24c35bb4013870301d47a20
Kalasen <kalasenzyphurus@yahoo.com>
2017-01-29 14:34:02 -0500

    Pulled generic game code from Adventurer to its own library

---
+++ Enums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaGame
+{
+    public enum Fonts
+    {
+        None,
+        Vera,
+        VeraSmall
+    }
+
+    public enum Directions
+    {
+        SW = 1,
+        S = 2,
+        SE = 3,
+        W = 4,
+        NONE = 5,
+        E = 6,
+        NW = 7,
+        N = 8,
+        NE = 9
+    }
+
+    public enum GameState
+    {
+        None,
+        OpeningMenu,
+        MainGame,
+        CreatureSelect,
+        HelpMenu,
+        EscapeMenu,
+        HealthMenu,
+        InventoryMenu,
+        NameSelect,
+        WaitForPosition
+    }
+}
---
+++ IDrawable.cs
@@ -0,0 +1,9 @@
+﻿namespace KalaGame
+{
+    //Represents something drawable to the screen
+    public interface IDrawable
+    {
+        //Draw this thing to the screen
+        void Draw(Graphics graphics);
+    }
+}
